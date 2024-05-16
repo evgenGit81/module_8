815abf93d3bc825f9c83f58d2694ab5c0b1a84d4
evgenGit81 <139344863+evgenGit81@users.noreply.github.com>
2024-05-16 18:36:31 +0000

    Add files via upload

---
+++ try_except_mod8_1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+def add_everything_up(a, b):
+    """Функция суммирования всего"""
+    try:
+        c = a + b
+        return c
+    except TypeError:
+        print("Несовпадение форматов, но я суммирую:")
+        bb = str(b)
+        aa = str(a)
+        c = aa + bb
+        return c
+
+
+print(add_everything_up(2, 8))
+print(add_everything_up(2, 123.765))
+print(add_everything_up(2, "4hzdfhzf"))
+print(add_everything_up("kg2", "4hzdfhzf"))
+print(add_everything_up("kg2", 67.98))
